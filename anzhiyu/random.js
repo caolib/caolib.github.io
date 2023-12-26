5f56dd2a8d9d0727e7b478bc5c7d89f73cfa15de
clb <1265501579@qq.com>
2023-12-26 22:54:20 +0800

    Site updated: 2023-12-26 22:54:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/07/28/C/","2023/08/31/Redis/","2023/07/28/git/","2023/08/31/linux命令/","2023/08/31/mybatis-plus/","2023/10/14/mybatis/","2023/07/28/操作系统/","2023/07/28/mysql/","2023/10/26/计算机网络/","2023/07/28/软件工程/"];function toRandomPost(){
+var posts=["2023/08/31/Redis/","2023/07/28/C/","2023/07/28/git/","2023/08/31/linux命令/","2023/08/31/mybatis-plus/","2023/10/14/mybatis/","2023/07/28/mysql/","2023/07/28/操作系统/","2023/10/26/计算机网络/","2023/07/28/软件工程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
